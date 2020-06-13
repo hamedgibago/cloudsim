45a74337bb398b707c2f7240a2271a5d7e124968
hamed_gibago <hamed_gibago@yahoo.com>
2020-06-14 01:34:32 +0430

    threadshold sample khodam

---
+++ modules/cloudsim/src/main/java/org/cloudbus/cloudsim/UtilizationModelThreshold.java
@@ -0,0 +1,26 @@
+package org.cloudbus.cloudsim;
+
+public class UtilizationModelThreshold implements UtilizationModel {
+
+	private double threshold;
+	
+	public UtilizationModelThreshold(double threshold)
+	{
+		this.threshold=threshold;
+	}
+	
+	@Override
+	public double getUtilization(double time) {	
+		// TODO Auto-generated method stub
+		return getThreshold();
+	}
+
+	public double getThreshold() {
+		return threshold;
+	}
+
+	public void setThreshold(double threshold) {
+		this.threshold = threshold;
+	}
+
+}
